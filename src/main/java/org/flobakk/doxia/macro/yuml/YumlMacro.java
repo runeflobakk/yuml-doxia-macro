76cde0ed8202c4bd91ae28ad23786c4d44c55705
Rune Flobakk <flobakk@gmail.com>
2010-10-02 19:20:45 +0200

    Refactored URL building from request params to use Apache Commons Exec

--- src/main/java/org/flobakk/doxia/macro/yuml/YumlMacro.java
+++ src/main/java/org/flobakk/doxia/macro/yuml/YumlMacro.java
@@ -1,7 +1,9 @@
 package org.flobakk.doxia.macro.yuml;
 
-import static org.apache.commons.lang.StringUtils.defaultString;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import org.apache.commons.exec.CommandLine;
 import org.apache.maven.doxia.macro.AbstractMacro;
 import org.apache.maven.doxia.macro.MacroExecutionException;
 import org.apache.maven.doxia.macro.MacroRequest;
@@ -13,22 +15,24 @@ import org.apache.maven.doxia.sink.Sink;
  */
 public class YumlMacro extends AbstractMacro {
 
-    private static final String BASE_URL = "http://yuml.me/diagram/";
-
-
 
     @Override
     public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
-        String model = (String)request.getParameter("model");
-        String type = defaultString((String)request.getParameter("type"), "class");
-        String style = defaultString((String)request.getParameter("style"), "scruffy");
-        String size = defaultString((String)request.getParameter("size"), "100");
-        String direction = defaultString((String)request.getParameter("direction"), "lr");
 
-        required("model", model);
+        @SuppressWarnings("unchecked")
+        ConcurrentMap<String, String> params = new ConcurrentHashMap<String, String>(request.getParameters());
+
+        required("model", params.get("model"));
+        params.putIfAbsent("type", "class");
+        params.putIfAbsent("style", "scruffy");
+        params.putIfAbsent("size", "100");
+        params.putIfAbsent("direction", "lr");
+
+        CommandLine url = new CommandLine("http://yuml.me/diagram/${style}/${type}/${model}");
+        url.setSubstitutionMap(params);
 
         sink.figure(null);
-        sink.figureGraphics(BASE_URL + style + "/" + type + "/" + model, null);
+        sink.figureGraphics(url.toString(), null);
         sink.figure_();
 
     }
